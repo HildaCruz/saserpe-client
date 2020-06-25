51a81d7a660ce2f4c4ca174ebeb3d621066dda05
Hilda Cruz <hcruz@walook.com.mx>
2020-06-24 20:30:38 -0500

    esqueleto de vistas

---
+++ application/views/auctions.php
@@ -0,0 +1,31 @@
+<?php
+?>
+
+<div class="container">
+    <div>
+        <table>
+            <tr>
+                <th>Compañía</th>
+                <th>RFC</th>
+                <th>Totales</th>
+                <th>Disponibles</th>
+                <th>Valor</th>
+            </tr>
+            <tr>
+                <td>Pannasoft</td>
+                <td>PANA120062020UAD</td>
+                <td>50</td>
+                <td>50</td>
+                <td>$10.50</td>
+            </tr>
+        </table>
+    </div>
+
+    <div>
+        <input placeholder="compañía">
+        <input placeholder="número de acciones">
+        <input placeholder="precio propuesto">
+        <button class="btn-icon">Compra</button>
+        <button class="btn-icon">Venta</button>
+    </div>
+</div>
