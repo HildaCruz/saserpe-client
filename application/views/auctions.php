c7c82eb824264591b63ff991d32d718dee1483e6
mbrito <mbrito@walook.com.mx>
2020-06-27 21:20:26 -0500

    Cambios en front-end

--- application/views/auctions.php
+++ application/views/auctions.php
@@ -1,48 +1,73 @@
 
-
+<link rel="stylesheet" href="<?php echo base_url();?>assets/css/auctions.css">
 <div class="container">
+<button id="btn_logout" type="button" class="btn btn-danger" onclick="logout()">Logout</button>
 <input style="display:none;" id="sesion" name = "sesion" value="<?= $this->session->userdata('id_sesion');?>">
 <input style="display:none;" id="rfc" name = "rfc" value="<?= $this->session->userdata('rfc_usuario');?>">
-    <div>
+<div id="principal" class="overflow-auto">
         <?php
         echo "Sesion ID ". $this->session->userdata('id_sesion');
          ?>
         <h1>Acciones</h1>
-        <table>
-            <tr>
-                <th>RFC Empresa</th>
-                <th>Acciones Totales</th>
-                <th>Acciones Disponibles</th>
-                <th>Precio de compra actual</th>
-            </tr>
-            <tbody id="companies"></tbody>
-        </table>
-    </div>
+        <div id = "tabla_empresas">
+            <table class="table">
+                <thead class="thead-light">
+                    <tr>
+                        <th scope="col">RFC Empresa</th>
+                        <th scope="col">Acciones Totales</th>
+                        <th scope="col">Acciones Disponibles</th>
+                        <th scope="col">Precio de compra actual</th>
+                    </tr>
+                </thead>
+                <tbody id="companies"></tbody>
+            </table>
+        </div>
     <br>
-    <div>
+    <div id="tabla_portafolio">
         <h1>Mi portafolio</h1>
-        <table>
-            <tr>
-                <th>RFC Empresa</th>
-                <th>Acciones compradas</th>
-                <th>Último precio de C/V ofertado</th>
-            </tr>
+        <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">RFC Empresa</th>
+                    <th scope="col">Acciones compradas</th>
+                    <th scope="col">Último precio de Compra ofertado</th>
+                </tr>
+            </thead>
             <tbody id="portafolio"></tbody>
         </table>
     </div>
     <br>
-    <div>
-        <div id="response_messages" class="message" style="color: blue"></div>
-        <br>
-        <input id="company" placeholder="compañía">
-        <input id="stocks" placeholder="número de acciones">
-        <input id="price_offer" placeholder="precio propuesto">
-        <button id="btn_compra" class="btn-icon" onclick="stocks('b')">Compra</button>
-        <button id="btn_venta" class="btn-icon" onclick="stocks('s')">Venta</button>
+    <div id="response_messages" class="message" style="color: blue"></div>
+    <div id = "mensaje_correcto" class="alert alert-success alert-dismissible fade show" role="alert">
+        <strong id="mensajeBueno"></strong>
+        <button id = "button_correcto" type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
     </div>
-    <button id="btn_logout" class="btn-icon" onclick="logout()">Logout</button>
+    <div id = "mensaje_error" class="alert alert-danger alert-dismissible fade show" role="alert">
+        <strong id="mensajeMalo"></strong>
+        <button id = "button_error" type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+    </div>
+
+    <form id="form_propuesta">
+        <h1>Realizar propuesta</h1>
+        <div class="form-group">
+            <label for="company">Empresa</label>
+            <input type="text" class="form-control" id="company" placeholder="Ingresa la compañia">
+        </div>
+        <div class="form-group">
+            <label for="stocks">No. Acciones</label>
+            <input type="number" class="form-control" id="stocks" placeholder="Número de acciones">
+        </div>
+        <div class="form-group">
+            <label for="price_offer">Precio propuesto</label>
+            <input type="number" class="form-control" id="price_offer" placeholder="Precio propuesto">
+        </div>
+    </form>
+    <button id = "btn_compra" type="button" class="btn btn-primary" onclick="stocks('b')">Compra</button>
+    <button id = "btn_venta"  type="button" class="btn btn-primary" onclick="stocks('s')">Venta</button>
+</div>
 </div>
 <script language="JavaScript" type="text/javascript" src="<?php echo base_url();?>assets/js/jquery-3.5.1.min.js"></script>
 <script language="JavaScript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 <script language="JavaScript" type="text/javascript" src="<?php echo base_url();?>assets/js/auctions.js"></script>
+
 
--- assets/js/auctions.js
+++ assets/js/auctions.js
@@ -37,20 +37,32 @@ function setupStocksEventSource() {
         if(data.tipoAccion === "C"){
             if (is_winner === true){
                 message = "Felicidades, ganaste acciones de la empresa " + data.empresa;
+                document.getElementById("mensajeBueno").innerHTML = message;
+                document.getElementById("mensaje_correcto").style.display = 'block';
+                $("#mensaje_correcto").delay(3200).fadeOut(3000);
             }
             else if (is_winner === false) {
                 message = "Lo sentimos, no ganaste acciones de la empresa " + data.empresa;
+                document.getElementById("mensajeMalo").innerHTML = message;
+                document.getElementById("mensaje_error").style.display = 'block';
+                $("#mensaje_error").delay(3200).fadeOut(3000);
             }
         }
         else if (data.tipoAccion=== "V") {
             if (is_winner === true){
                 message = "Felicidades, vendiste acciones de la empresa " + data.empresa;
+                document.getElementById("mensajeBueno").innerHTML = message;
+                document.getElementById("mensaje_correcto").style.display = 'block';
+                $("#mensaje_correcto").delay(3200).fadeOut(3000);
             }
             else if (is_winner === false) {
                 message = "Lo sentimos, no vendiste acciones de la empresa " + data.empresa;
+                document.getElementById("mensajeMalo").innerHTML = message;
+                document.getElementById("mensaje_error").style.display = 'block';
+                $("#mensaje_error").delay(3200).fadeOut(3000);
             }
         }
-         $("#response_messages").append(message);
+         //$("#response_messages").append(message);
     }, false);
     source.addEventListener('actualizar-propuesta', function(e) {
          console.log(e.data);
@@ -142,13 +154,20 @@ function buyStocks(company,stocks,price_offer){
         data: JSON.stringify(request),
         success: function (response) {
             console.log(response);
-            $("#response_messages").append(response.mensaje);
+            document.getElementById("mensajeBueno").innerHTML = response.mensaje;
+            document.getElementById("mensaje_correcto").style.display = 'block';
+            cleanData();
+            $("#mensaje_correcto").delay(3200).fadeOut(3000);
+            //$("#response_messages").append(response.mensaje);
 
         },
         error : function(error) {
             console.log(error);
             var message = error.responseJSON;
-            $("#response_messages").append(message.mensaje);//sin lo de json parse el index era mensaje, responseText es donde esta el mensaje
+            document.getElementById("mensajeMalo").innerHTML = message.mensaje;
+            document.getElementById("mensaje_error").style.display = 'block';
+            $("#mensaje_error").delay(3200).fadeOut(3000);
+            //$("#response_messages").append(message.mensaje);//sin lo de json parse el index era mensaje, responseText es donde esta el mensaje
         }
     });
 }
@@ -168,17 +187,27 @@ function sellStocks(company,stocks,price_offer){
         data: JSON.stringify(request),
         success: function (response) {
             console.log(response);
-            var message = JSON.parse(response);
-            $("#response_messages").append(message.responseText);
+            document.getElementById("mensajeBueno").innerHTML = response.mensaje;
+            document.getElementById("mensaje_correcto").style.display = 'block';
+            cleanData();
+            $("#mensaje_correcto").delay(3200).fadeOut(3000);
         },
         error : function(error) {
             console.log(error);
-            var message = JSON.parse(error);
-            $("#response_messages").append(message.mensaje);//lo mismo aca, si jala en compra jala en venta
+            var message = error.responseJSON;
+            document.getElementById("mensajeMalo").innerHTML = message.mensaje;
+            document.getElementById("mensaje_error").style.display = 'block';
+            $("#mensaje_error").delay(3200).fadeOut(3000);
+            //$("#response_messages").append(message.mensaje);//lo mismo aca, si jala en compra jala en venta
         }
     });
 }
 function logout(){
     window.location.href = 'http://localhost/saserpe-client/login/logout';
+}
+function cleanData(){
+    document.getElementById("company").value='';
+    document.getElementById("stocks").value='';
+    document.getElementById("price_offer").value='';
 }
 init();
